6d7b283589d88d000b06ffff513f1fc4efc2784e
jonasfernandes <jonas.amilk@gmail.com>
2025-11-24 14:18:06 -0300

    feat: remove fixed language value on Months component and add value from i18n context

--- src/components/github/Months.tsx
+++ src/components/github/Months.tsx
@@ -3,9 +3,11 @@
 import { useGithubDataContext } from '@/context/GithubDataContext';
 import { GitContributionMonth } from '@/types/github';
 import { normalizeMonthStyle } from '@/utils/date';
+import { useI18nContext } from '@/context/I18nContext';
 
 export default function Months() {
   const { weeks, months } = useGithubDataContext();
+  const { currentLanguage } = useI18nContext();
 
   const getMonthOffset = (firstDay: string) => {
     const monthWeekDay = weeks.reduce((acc: number, week) => {
@@ -23,7 +25,7 @@ export default function Months() {
   };
 
   const getTranslatedMonth = (month: GitContributionMonth) => {
-    const format = new Intl.DateTimeFormat('en-US', { month: 'short', timeZone: 'UTC' });
+    const format = new Intl.DateTimeFormat(currentLanguage, { month: 'short', timeZone: 'UTC' });
     const result = format.format(new Date(month.firstDay));
     return normalizeMonthStyle(result);
   };
