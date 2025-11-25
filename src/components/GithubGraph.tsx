b2c881e632290c82f5458c0ebce94ffa2f198abf
jonasfernandes <jonas.amilk@gmail.com>
2025-11-25 11:35:48 -0300

    feat: use new translations for github Graph component

--- src/components/GithubGraph.tsx
+++ src/components/GithubGraph.tsx
@@ -6,9 +6,12 @@ import { GithubDataProvider, useGithubDataContext } from '@/context/GithubDataCo
 import EmptyState from './EmptyState';
 import { getGitHubYears } from '@/utils/githubContribution';
 import Slide from '@/components/effects/Slide';
+import { useTranslation } from 'react-i18next';
 
 function ContributionGraphContent() {
   const { calendarYear, setCalendarYear, error } = useGithubDataContext();
+  const { t } = useTranslation();
+
   const joinYear = Number(import.meta.env.VITE_GITHUB_JOIN_YEAR);
   const years = getGitHubYears(joinYear);
 
@@ -19,7 +22,7 @@ function ContributionGraphContent() {
   return (
     <section className="flex flex-col gap-4 lg:items-center">
       <Slide delay={0.5}>
-        <p className="font-[Proxima_Nova_Bold] text-3xl text-foreground-2">GitHub Contributions</p>
+        <p className="font-[Proxima_Nova_Bold] text-3xl text-foreground-2">{t('github.title')}</p>
         <div className="flex flex-col xl:flex-row gap-4 mt-4">
           <div className="bg-github-container border dark:border-zinc-800 border-zinc-200 p-8 rounded-lg max-w-fit max-h-fit">
             <GitHubCalendar />
