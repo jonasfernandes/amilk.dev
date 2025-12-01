24cf523aa27747e3718ecf58978b925344b4602b
jonasfernandes <jonas.amilk@gmail.com>
2025-12-01 11:58:42 -0300

    feat: add loader data profile from ssr

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,9 +2,23 @@ import { createFileRoute } from '@tanstack/react-router';
 
 import Hero from '@/components/Hero';
 import ContributionGraph from '@/components/GithubGraph';
+import { getProfile } from '@/services/profile';
+import { LocalStorage } from '@/utils/localStorage';
+import { storageKeys } from '@/utils/constants/storageKeys';
+import type { Profile } from '@/types/profile';
+
+async function fetchProfile(): Promise<Profile> {
+  const currentLanguage = LocalStorage.get(storageKeys.LANGUAGE);
+
+  const tempProfile = await getProfile();
+  const [description] = tempProfile.description.filter((item) => item._key === currentLanguage);
+
+  return { ...tempProfile, description: description.value };
+}
 
 export const Route = createFileRoute('/')({
   component: App,
+  loader: fetchProfile,
 });
 
 function App() {
