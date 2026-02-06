4e551bf0a96e3016cf83ad8185e7f8f8bdf57c4c
jonasfernandes <jonas.amilk@gmail.com>
2026-02-06 16:29:33 -0300

    fix: remove profile context and adjust logic for getProfile service

--- src/services/profile.ts
+++ src/services/profile.ts
@@ -1,10 +1,17 @@
-import type { ProfileApi } from '@/types/profile';
+import type { Profile, ProfileApi } from '@/types/profile';
 import { client } from '@/sanityClient';
+import { storageKeys } from '@/utils/constants/storageKeys';
+import { LocalStorage } from '@/utils/localStorage';
 
 const PROFILE_QUERY = `*[
   _type == "profile"
 ][0]`;
 
-export async function getProfile(): Promise<ProfileApi> {
-  return await client.fetch<ProfileApi>(PROFILE_QUERY);
+export async function getProfile(): Promise<Profile> {
+  const currentLanguage = LocalStorage.get(storageKeys.LANGUAGE);
+  const profile = await client.fetch<ProfileApi>(PROFILE_QUERY);
+
+  const [description] = profile.description.filter((item) => item._key === currentLanguage);
+
+  return { ...profile, description: description.value };
 }
