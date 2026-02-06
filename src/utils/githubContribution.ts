ff034ff4ddf8f3ff9d02b5d23ee12c02ad25959c
jonasfernandes <jonas.amilk@gmail.com>
2026-02-06 19:54:47 -0300

    fix: adjust date on fake data to match the correct months and days on loading

--- src/utils/githubContribution.ts
+++ src/utils/githubContribution.ts
@@ -1,4 +1,5 @@
 import { githubKeysLevel } from '@/utils/constants/githubKeys';
+import { memo } from 'motion/react';
 
 export function getGitHubYears(joinYear: number | undefined): number[] {
   if (!joinYear) return [];
@@ -9,22 +10,47 @@ export function getGitHubYears(joinYear: number | undefined): number[] {
   return years;
 }
 
-export function getFakeContribution() {
-  return Array.from({ length: 54 }, () => ({
-    firstDay: new Date().toISOString(),
-    contributionDays: Array.from({ length: 7 }, (_, index) => ({
-      contributionCount: Math.floor(Math.random() * 10),
-      contributionLevel: githubKeysLevel.NONE,
-      date: new Date().toISOString(),
-      weekday: index,
-    })),
-  }));
+let multipleDay = 0;
+
+function getCorrectMonth(index: number) {
+  return String(index).padStart(2, '0');
 }
 
-export function getFakeMonths() {
-  return Array.from({ length: 12 }, () => ({
+function getCorrectDay(index: number) {
+  const day = index - 31 * multipleDay;
+
+  if (index % 31 === 0) {
+    multipleDay++;
+  }
+  return String(day).padStart(2, '0');
+}
+
+export const getFakeWeeks = memo(function () {
+  multipleDay = 0;
+  let month = 1;
+  let days = 1;
+
+  return Array.from({ length: 53 }, (_, indexArray) => ({
     firstDay: new Date().toISOString(),
+    contributionDays: Array.from({ length: 7 }, (_, index) => {
+      days = 7 * indexArray + (index + 1);
+      const tmpMonth = month;
+      if (days % 31 === 0) month++;
+
+      return {
+        contributionCount: Math.floor(Math.random() * 10),
+        contributionLevel: githubKeysLevel.NONE,
+        date: `${new Date().getFullYear()}-${getCorrectMonth(tmpMonth)}-${getCorrectDay(days)}T00:00:00Z`,
+        weekday: index,
+      };
+    }),
+  }));
+});
+
+export const getFakeMonths = memo(function () {
+  return Array.from({ length: 12 }, (_, index: number) => ({
+    firstDay: `${new Date().getFullYear()}-${getCorrectMonth(index + 1)}-01T00:00:00Z`,
     name: '',
     year: 0,
   }));
-}
+});
