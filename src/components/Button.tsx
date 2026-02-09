8e59f3448b45152439292ed612c92e90320fbebe
jonasfernandes <jonas.amilk@gmail.com>
2026-02-09 14:57:13 -0300

    feat: add hover animation with gsap on button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,6 @@
-import { MouseEventHandler } from 'react';
+import { MouseEventHandler, useEffect, useRef } from 'react';
 import Magnetic from '@/components/effects/Magnetic';
+import gsap from 'gsap';
 
 export default function Button({
   children,
@@ -10,16 +11,44 @@ export default function Button({
   active?: boolean;
   onClick?: MouseEventHandler<HTMLButtonElement>;
 }) {
+  const circle = useRef(null);
+  const timeline = useRef<GSAPTimeline>(null);
+  let timeoutId: ReturnType<typeof setTimeout>;
+
+  useEffect(() => {
+    timeline.current = gsap.timeline({ paused: true });
+    timeline.current
+      .to(circle.current, { top: '-25%', width: '150%', duration: 0.4, ease: 'power3.in' }, 'enter')
+      .to(circle.current, { top: '-150%', width: '125%', duration: 0.25 }, 'exit');
+  }, []);
+
+  const manageMouseEnter = () => {
+    if (timeoutId) clearTimeout(timeoutId);
+    timeline.current?.tweenFromTo('enter', 'exit');
+  };
+
+  const manageMouseLeave = () => {
+    timeoutId = setTimeout(() => {
+      timeline.current?.play();
+    }, 300);
+  };
+
   return (
     <Magnetic>
       <button
-        onClick={onClick}
-        className={`w-full cursor-pointer rounded-lg text-center px-4 py-2 text-sm font-medium ${
+        className={`overflow-hidden relative w-full cursor-pointer rounded-lg text-center px-4 py-2 ${
           active ? 'bg-primary text-white' : 'bg-background-2 text-foreground'
-        }`}
+        } hover:text-white transition-colors duration-400 ease-in`}
+        onMouseEnter={manageMouseEnter}
+        onMouseLeave={manageMouseLeave}
+        onClick={onClick}
         title={`View Graph for the year ${children}`}
       >
-        {children}
+        <p className="z-10 relative text-sm font-medium">{children}</p>
+        <div
+          ref={circle}
+          className="absolute top-full left-1/2 w-full h-[150%] rounded-[50%] bg-primary transform -translate-x-1/2"
+        ></div>
       </button>
     </Magnetic>
   );
