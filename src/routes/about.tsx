0e33acf967a76303e48389dc8e4d2b55985d2762
jonasfernandes <jonas.amilk@gmail.com>
2026-02-25 13:28:43 -0300

    feat: new styles for work experience in about page

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -13,19 +13,17 @@ export const Route = createFileRoute('/about')({
 
 function About() {
   const jobs = use(getJobsPromise);
-  console.log(jobs);
 
   return (
     <Wrapper>
       <h1 className="text-primary mb-8 text-3xl font-bold">About Me</h1>
       <p className="text-foreground-2 mb-4">
-        I'm a passionate software developer with experience in building web applications using
-        modern technologies. I enjoy learning new programming languages and frameworks, and I'm
+        I&apos;m a passionate software developer with experience in building web applications using
+        modern technologies. I enjoy learning new programming languages and frameworks, and I&apos;m
         always looking for ways to improve my skills.
       </p>
-
       <h2 className="text-foreground-2 mb-4 text-2xl font-bold">Work Experience</h2>
-      <ul className="list-inside list-disc">
+      <ul className="grid list-inside list-disc gap-6 md:grid-cols-2 md:gap-12">
         {jobs.map((job) => {
           const from = formatDateToLocale(job.from, { month: 'short', year: 'numeric' });
           const to = formatDateToLocale(job.to, { month: 'short', year: 'numeric' });
@@ -35,10 +33,23 @@ function About() {
             : undefined;
 
           return (
-            <li key={job._id} className="text-foreground-2 mb-2">
-              <img className="h-24 w-24 rounded-md object-cover" src={imageJobUrl} />
-              <strong>{job.title}</strong> at {job.company} ({from}) - {to}
-              <p>{job.description}</p>
+            <li
+              key={job._id}
+              className="text-foreground-2 relative flex max-w-2xl items-start gap-x-8"
+            >
+              <div className="bg-github-container border-secondary after:bg-secondary relative grid min-h-20 min-w-20 place-items-center rounded-md border after:absolute after:top-[50%] after:left-19.75 after:h-px after:w-4">
+                <img className="h-13 w-13 rounded-sm object-cover" src={imageJobUrl} />
+              </div>
+              <div className="before:bg-secondary relative flex h-full w-full flex-col items-start before:absolute before:top-0 before:-left-4 before:h-full before:w-px">
+                <strong className="text-foreground font-mono text-xl tracking-tighter">
+                  {job.company}
+                </strong>
+                <strong className="text-foreground">{job.title}</strong>
+                <p className="mt-2 text-sm tracking-widest text-zinc-500 uppercase">
+                  {from} - {to}
+                </p>
+                <p className="my-4 tracking-tight">{job.description}</p>
+              </div>
             </li>
           );
         })}
