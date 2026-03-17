53df2e720b42da39d3ddb8de51fce282578568a7
jonasfernandes <jonas.amilk@gmail.com>
2026-03-17 09:58:16 -0300

    fix: remove uncenessary code from ThemeSelector, now we have initial theme value

--- src/components/ThemeSelector.tsx
+++ src/components/ThemeSelector.tsx
@@ -4,11 +4,9 @@ import SunIcon from '@/assets/icons/Sun';
 import MoonIcon from '@/assets/icons/Moon';
 import Magnetic from '@/components/effects/Magnetic';
 import { useThemeStore } from '@/store/theme';
-import { useIsMounted } from '@/hooks/useIsMounted';
 
 export default function ThemeSelector() {
   const { theme, setTheme } = useThemeStore();
-  const isMounted = useIsMounted();
 
   function updateTheme() {
     setTheme(theme === 'dark' ? 'light' : 'dark');
@@ -23,13 +21,6 @@ export default function ThemeSelector() {
       document.documentElement.classList.remove('dark');
     }
   }, [theme]);
-
-  if (!isMounted)
-    return (
-      <span className="p-2">
-        <span className="bg-foreground/10 border-foreground/30 flex h-5.5 w-5.5 animate-pulse rounded-full border"></span>
-      </span>
-    );
 
   return (
     <Magnetic>
