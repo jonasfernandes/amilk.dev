f0799cd7d2469bb69ba7891e7a9234f390a98109
jonasfernandes <jonas.amilk@gmail.com>
2026-03-23 11:13:00 -0300

    fix: adjust mobile layout for contact section

--- src/features/contact/index.tsx
+++ src/features/contact/index.tsx
@@ -3,6 +3,7 @@ import { useScroll, motion, useTransform } from 'framer-motion';
 import Button from '@/components/Button';
 import { getProfile } from '@/services/profile';
 import { urlFor } from '@/utils/sanityImageUrl';
+import Wrapper from '@/components/Wrapper';
 
 const getProfilePromise = getProfile();
 
@@ -23,52 +24,54 @@ export default function Contact() {
     <motion.div
       style={{ y }}
       ref={container}
-      className="bg-background text-foreground-2 bg-noise relative flex flex-col items-center justify-center"
+      className="bg-background text-foreground-2 bg-noise relative"
     >
-      <div className="w-full pt-28 pb-28 sm:pt-48 sm:pb-48">
-        <div className="border-foreground-2 relative mr-20 ml-20 border-b pb-24 sm:mr-48 sm:ml-48">
-          <span className="flex items-center gap-4">
-            <div className="relative h-24 w-24 overflow-hidden rounded-full">
-              <img className="object-cover" alt={'image'} src={imageProfileUrl} />
-            </div>
-            <h2 className="m-0 text-6xl font-light">Let&apos;s work</h2>
-          </span>
-          <h2 className="m-0 text-6xl font-light">together</h2>
-          <motion.div
-            style={{ x }}
-            className="absolute top-[calc(100%-75px)] left-[calc(100%-400px)] z-10"
-          >
-            <Button
-              reverse
-              customButtonStyles="w-48 h-48 absolute rounded-full"
-              customFontStyles="text-base font-light relative"
+      <Wrapper>
+        <div className="w-full pt-28 pb-28 sm:pt-48 sm:pb-48">
+          <div className="border-foreground-2 relative border-b pb-24">
+            <span className="flex items-center gap-4">
+              <div className="relative h-24 w-24 overflow-hidden rounded-full">
+                <img className="object-cover" alt={'image'} src={imageProfileUrl} />
+              </div>
+              <h2 className="m-0 text-6xl font-light">Let&apos;s work</h2>
+            </span>
+            <h2 className="m-0 text-6xl font-light">together</h2>
+            <motion.div
+              style={{ x }}
+              className="absolute top-[calc(100%-40px)] left-[calc(100%-240px)] z-10 sm:top-[calc(100%-75px)] sm:left-[calc(100%-400px)]"
             >
-              Get in touch
-            </Button>
-          </motion.div>
-          <motion.svg
-            style={{ rotate: 90, scale: 2 }}
-            width="9"
-            height="9"
-            viewBox="0 0 9 9"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className="absolute top-[30%] left-full"
-          >
-            <path
-              d="M8 8.5C8.27614 8.5 8.5 8.27614 8.5 8L8.5 3.5C8.5 3.22386 8.27614 3 8 3C7.72386 3 7.5 3.22386 7.5 3.5V7.5H3.5C3.22386 7.5 3 7.72386 3 8C3 8.27614 3.22386 8.5 3.5 8.5L8 8.5ZM0.646447 1.35355L7.64645 8.35355L8.35355 7.64645L1.35355 0.646447L0.646447 1.35355Z"
-              fill="white"
-            />
-          </motion.svg>
-        </div>
-        <div className="mt-24 mr-48 ml-48 flex gap-5">
-          <div>
-            <Button customButtonStyles="rounded-3xl cursor-pointer relative flex items-center justify-center py-3.5  px-16">
-              jonas.amilk@gmail.com
-            </Button>
+              <Button
+                reverse
+                customButtonStyles="sm:h-45 sm:w-45 h-28 w-28 absolute rounded-full"
+                customFontStyles="text-base font-light relative"
+              >
+                Get in touch
+              </Button>
+            </motion.div>
+            <motion.svg
+              style={{ rotate: 90, scale: 2 }}
+              width="9"
+              height="9"
+              viewBox="0 0 9 9"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="absolute top-[30%] hidden sm:left-full"
+            >
+              <path
+                d="M8 8.5C8.27614 8.5 8.5 8.27614 8.5 8L8.5 3.5C8.5 3.22386 8.27614 3 8 3C7.72386 3 7.5 3.22386 7.5 3.5V7.5H3.5C3.22386 7.5 3 7.72386 3 8C3 8.27614 3.22386 8.5 3.5 8.5L8 8.5ZM0.646447 1.35355L7.64645 8.35355L8.35355 7.64645L1.35355 0.646447L0.646447 1.35355Z"
+                fill="white"
+              />
+            </motion.svg>
+          </div>
+          <div className="mt-24 mr-48 flex">
+            <div>
+              <Button customButtonStyles="rounded-3xl cursor-pointer relative flex items-center justify-center py-3.5  px-16">
+                jonas.amilk@gmail.com
+              </Button>
+            </div>
           </div>
         </div>
-      </div>
+      </Wrapper>
     </motion.div>
   );
 }
